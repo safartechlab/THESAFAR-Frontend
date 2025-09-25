0a38d01b5e94fdd2f9768c242ddd1bc83dbddfe8
safartechlab <safartechlab@gmail.com>
2025-09-25 14:06:47 +0530

    Resolved merge conflicts

--- src/pages/admin/Subcategory/updatesubcategory.jsx
+++ src/pages/admin/Subcategory/updatesubcategory.jsx
@@ -1,13 +1,16 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { Baseurl } from "../../../baseurl"
-import { showToast } from "../../../store/slice/toast_slice"
-import { closesubupdate, getsubcate } from "../../../store/slice/Subcategoryslice"
-import { Modal, Form } from "react-bootstrap"
-import { getcategory } from "../../../store/slice/category_slice"
-import { getsize } from "../../../store/slice/Sizeslice"
-import { MultiSelect } from "primereact/multiselect"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Baseurl } from "../../../baseurl";
+import { showToast } from "../../../store/slice/toast_slice";
+import {
+  closesubupdate,
+  getsubcate,
+} from "../../../store/slice/Subcategoryslice";
+import { Modal, Form } from "react-bootstrap";
+import { getcategory } from "../../../store/slice/category_slice";
+import { getsize } from "../../../store/slice/Sizeslice";
+// import { MultiSelect } from "primereact/multiselect";
 
 const Updatesubcategory = () => {
   const dispatch = useDispatch();
@@ -20,9 +23,9 @@ const Updatesubcategory = () => {
   const [subcate, setsubcate] = useState({
     id: "",
     subcategory: "",
-    categoryID: "",
-    sizes: [],
-  })
+    category: "",
+    sizes: [], // keep only IDs
+  });
 
   // fetch category and size lists
   useEffect(() => {
@@ -32,34 +35,27 @@ const Updatesubcategory = () => {
 
   // populate data when subcategorydata changes
   useEffect(() => {
-    if (subcategorydata) {
-      setsubcate({
-        id: subcategorydata._id || "",
-        subcategory: subcategorydata.subcategory || "",
-        categoryID: subcategorydata.categoryID || "",
-        sizes:
-          subcategorydata.sizes?.map((s) => {
-            // match with size list to get correct label
-            const matched = size.find((sz) => sz._id === s._id || sz._id === s)
-            return {
-              label: matched?.sizename || matched?.size || "",
-              value: matched?._id || s._id || s,
-            }
-          }) || [],
-      })
-    }
-  }, [subcategorydata, size])
+  if (subcategorydata) {
+    setsubcate({
+      id: subcategorydata._id || "",
+      subcategory: subcategorydata.subcategory || "",
+      category: subcategorydata.categoryID || subcategorydata.category?._id || "", 
+      sizes: subcategorydata.sizes?.map((s) => s._id || s) || [],
+    });
+  }
+}, [subcategorydata]);
 
-  // handle text/option changes
+
+  // handle text input
   const handlesubcatechange = (e) => {
     const { name, value } = e.target;
     setsubcate({ ...subcate, [name]: value });
   };
 
-  // handle multiselect changes
-  const handleSizesChange = (e) => {
-    setsubcate({ ...subcate, sizes: e.value })
-  }
+  // handle MultiSelect
+  // const handleSizesChange = (e) => {
+  //   setsubcate({ ...subcate, sizes: e.value }); // IDs only
+  // };
 
   // update API call
   const update = async () => {
@@ -94,73 +90,85 @@ const Updatesubcategory = () => {
   };
 
   return (
-    <>
-      <Modal show={!!subcategorydata} onHide={() => dispatch(closesubupdate())}>
-        <Modal.Header>
-          <Modal.Title>Update Sub Category</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            {/* Subcategory Name */}
-            <Form.Group className="mb-3" controlId="subcategory">
-              <Form.Label>Sub Category Name</Form.Label>
-              <Form.Control
-                type="text"
-                name="subcategory"
-                placeholder="Enter Subcategory Name"
-                value={subcate.subcategory}
-                onChange={handlesubcatechange}
-              />
-            </Form.Group>
+    <Modal show={!!subcategorydata} onHide={() => dispatch(closesubupdate())}>
+      <Modal.Header>
+        <Modal.Title>Update SubCategory</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          {/* Subcategory Name */}
+          <Form.Group className="mb-3" controlId="subcategory">
+            <Form.Label>Sub Category Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="subcategory"
+              placeholder="Enter Subcategory Name"
+              value={subcate.subcategory}
+              onChange={handlesubcatechange}
+            />
+          </Form.Group>
 
           {/* Category Dropdown */}
           <Form.Group controlId="category" className="mt-3">
             <Form.Label>Select Category</Form.Label>
             <Form.Select
-              name="category"
-              value={subcate.category}
-              onChange={handlesubcatechange}
-            >
-              <option value="">-- Select Category --</option>
-              {category.map((cate) => (
-                <option value={cate._id} key={cate._id}>
-                  {cate.categoryname}
-                </option>
-              ))}
-            </Form.Select>
+  name="category"
+  value={subcate.category}
+  onChange={handlesubcatechange}
+>
+  <option value="">-- Select Category --</option>
+  {category.map((cate) => (
+    <option value={cate._id} key={cate._id}>
+      {cate.categoryname || cate.category}
+    </option>
+  ))}
+</Form.Select>
+
           </Form.Group>
 
-            {/* Sizes MultiSelect */}
-            <Form.Group controlId="sizes" className="mt-3">
-              <Form.Label>Select Sizes</Form.Label>
-              <MultiSelect
-                value={subcate.sizes}
-                options={size.map((s) => ({
-                  label: s.sizename || s.size,
-                  value: s._id,
-                }))}
-                onChange={handleSizesChange}
-                placeholder="Select Sizes"
-                display="chip"
-                className="w-100"
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <button className="rounded-2" onClick={() => dispatch(closesubupdate())}>
-            Close
-          </button>
-          <button
-            className="rounded-2 sidebar-color text-light"
-            onClick={update}
-          >
-            Update
-          </button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  )
-}
+          {/* Sizes MultiSelect */}
+          <Form.Group controlId="sizes" className="mt-3">
+            <Form.Label>Select Sizes</Form.Label>
+            <div>
+              {size.map((s) => (
+                <Form.Check
+                  key={s._id}
+                  type="checkbox"
+                  label={s.size}
+                  value={s._id}
+                  checked={subcate.sizes.includes(s._id)}
+                  onChange={(e) => {
+                    if (e.target.checked) {
+                      setsubcate({
+                        ...subcate,
+                        sizes: [...subcate.sizes, s._id],
+                      });
+                    } else {
+                      setsubcate({
+                        ...subcate,
+                        sizes: subcate.sizes.filter((id) => id !== s._id),
+                      });
+                    }
+                  }}
+                />
+              ))}
+            </div>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <button
+          className="rounded-2"
+          onClick={() => dispatch(closesubupdate())}
+        >
+          Close
+        </button>
+        <button className="rounded-2 sidebar-color text-light" onClick={update}>
+          Update
+        </button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
-export default Updatesubcategory
+export default Updatesubcategory;
